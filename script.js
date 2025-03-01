5a70393e9376b4de2a555bbbc932f5933e5d17cc
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-01 16:59:43 +0600

    added live time

--- script.js
+++ script.js
@@ -14,7 +14,30 @@ for (let button of buttons) {
         Assigned++;
         AssignedCount.textContent = Assigned;
     }
+    let cardTitle = event.target.parentNode.parentNode.querySelector(".cardTitle").textContent
+    const activityHistory = document.getElementById("activityHistory")
+    let currentDate = new Date()
+      .toLocaleDateString("en-US", {
+        weekday: "short", 
+        month: "short", 
+        day: "numeric", 
+        year: "numeric", 
+      }).replace(/,/, ", ");
     
-    
+    let currentTimeHistory = new Date().toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+    const div = document.createElement('div')
+    div.innerHTML = `
+    <p class="text-gray-500">You have completed the task ${cardTitle} at ${currentTimeHistory}</p>
+    `;
+    activityHistory.appendChild((div))
+
+    document.getElementById("clearHistory").addEventListener('click', function () {
+      document.getElementById("activityHistory").textContent = ''
+    })
   });
 }
