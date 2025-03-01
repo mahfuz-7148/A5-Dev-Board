afa71f610097b0dd8999f9d4841db16c149aca7f
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-01 23:15:31 +0600

    added color button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let buttons = document.querySelectorAll(".button");
 for (let button of buttons) {
   button.addEventListener("click", function (event) {
+    window.alert('Board updated successfully')
     event.target.setAttribute("disabled", true);
     let taskAssignedCount = document.querySelector(".taskAssignedCount");
       let tasksAssigned = Number(taskAssignedCount.textContent); 
@@ -16,20 +17,20 @@ for (let button of buttons) {
     }
     let cardTitle = event.target.parentNode.parentNode.querySelector(".cardTitle").textContent
     const activityHistory = document.getElementById("activityHistory")
-    let currentDate = new Date()
-      .toLocaleDateString("en-US", {
-        weekday: "short", 
-        month: "short", 
-        day: "numeric", 
-        year: "numeric", 
-      }).replace(/,/, ", ");
-    
+
+   
+  
+
+
+
     let currentTimeHistory = new Date().toLocaleTimeString("en-US", {
       hour: "2-digit",
       minute: "2-digit",
       second: "2-digit",
       hour12: true,
     });
+
+    
     const div = document.createElement('div')
     div.innerHTML = `
     <p class="text-gray-500">You have completed the task ${cardTitle} at ${currentTimeHistory}</p>
@@ -41,3 +42,13 @@ for (let button of buttons) {
     })
   });
 }
+
+document.getElementById("changeColor").addEventListener('click', function () {
+  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+
+ 
+  document.body.style.backgroundColor = randomColor;
+})
+
